10a7e66917d0c6c6d4c301279468e7790cd3dcd0
SE151289 <dathtse151289@fpt.edu.vn>
2024-01-23 00:20:33 +0700

    [FE017] create news screen

--- src/Screens/News/NewsScreen.tsx
+++ src/Screens/News/NewsScreen.tsx
@@ -50,121 +50,129 @@ export default function NewsScreen() {
       id: 1,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2Fa29ad4155f997fd8fdbed2172d114254.jpg?alt=media&token=3d8f906e-f4f7-4b4d-b533-e6b38d891611",
-      title: "Fashion",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "A Rummage Through Gucci's Archive, As Seen in AnOther",
     },
     {
       id: 2,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F366972237_619089963697280_9065720426821190201_n.jpg?alt=media&token=6af1d30c-8fb0-4e27-8104-8add7378c34e",
-      title: "Viet Nam",
-      description: "abc, def",
+      title: "ANOTHER THING I WANTED TO TELL YOU",
+      description:
+        "Designer Duran Lantink on the 'Extreme Energy' of Walter Van Beirendonck",
     },
     {
       id: 3,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "International",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 4,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "Beauty",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 5,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "Art",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 6,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "Design",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 7,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "History",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 8,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
-      title: "Title 8",
-      description: "abc, def",
+      title: "behind the pages",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 9,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 9",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 10,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 10",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 11,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 11",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 12,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 12",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 13,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 13",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 14,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 14",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 15,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 15",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
     {
       id: 16,
       image:
         "https://firebasestorage.googleapis.com/v0/b/whear-app-1f70d.appspot.com/o/Stuff%2F380438459_847206396710593_1080732157510625571_n.jpg?alt=media&token=961c3244-5235-44bd-b55e-97e27192e033",
       title: "Title 16",
-      description: "abc, def",
+      description: "Alice Neale's Sultry Portrait of Dior A/W23",
     },
   ];
 
   const renderItem = ({ item }: { item: NewsItems }) => (
     <View style={NewsStyle.itemContainer}>
       <Image source={{ uri: item.image }} style={NewsStyle.itemImage} />
-      <Text style={NewsStyle.itemTitle}>{item.title}</Text>
-      <Text style={NewsStyle.itemDescription}>{item.description}</Text>
+      <Text style={NewsStyle.itemTitle} onPress={() => alert("Read more")}>
+        {item.title}
+      </Text>
+      <Text
+        style={NewsStyle.itemDescription}
+        onPress={() => alert("Read more")}
+      >
+        {item.description}
+      </Text>
     </View>
   );
 
@@ -184,7 +192,13 @@ export default function NewsScreen() {
           <Chip
             key={i.id}
             mode="outlined"
-            onPress={() => console.log("Pressed")}
+            onPress={() => alert("Handle filter")}
+            textStyle={{
+              opacity: 0.8,
+              fontWeight: "400",
+              fontSize: 10,
+            }}
+            style={NewsStyle.chipStyles}
           >
             {i.title}
           </Chip>
@@ -195,12 +209,14 @@ export default function NewsScreen() {
         data={newsItems}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
-        numColumns={4}
+        numColumns={2}
         columnWrapperStyle={NewsStyle.row}
       />
 
       <View style={NewsStyle.loadMoreArticle}>
-        <Text style={NewsStyle.loadMore}>Load More Article</Text>
+        <Text style={NewsStyle.loadMore} onPress={() => alert("Loading....")}>
+          Load More Article
+        </Text>
       </View>
     </View>
   );
