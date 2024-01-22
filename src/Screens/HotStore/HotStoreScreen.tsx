38a730765b230a6b6dc187c5e1821eb49346b9ac
Thu Trang Le <lthutrang17@gmail.com>
2024-01-23 00:18:55 +0700

    [FE018] create hot store screen

--- src/Screens/HotStore/HotStoreScreen.tsx
+++ src/Screens/HotStore/HotStoreScreen.tsx
@@ -1,66 +1,212 @@
+import MaskedView from '@react-native-masked-view/masked-view';
+import { LinearGradient } from 'expo-linear-gradient';
 import React, { useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { Appbar, Avatar, Divider, Text } from 'react-native-paper';
+import IconFA from 'react-native-vector-icons/FontAwesome';
+import AppBarFooterComponents from '../../components/Common/AppBarFooter/AppBarFooterComponents';
 import AppBarHeaderComponent from '../../components/Common/AppBarHeader/AppBarHeaderComponent';
-import { Appbar } from 'react-native-paper';
+import ListViewComponent from '../../components/ListView/ListViewComponent';
+import { primaryColor, secondaryColor } from '../../root/Colors';
 import HotStoreStyleScreen from './HotStoreStyleScreen';
 
+interface Clothes {
+  clothesId: string;
+  clothesImgUrl: string;
+}
+interface HotStore {
+  brandId: string;
+  brandImgUrl: string;
+  brandName: string;
+  clothes: Clothes[];
+}
+interface StoreData {
+  data: HotStore[];
+}
+const HOST_STORE_DATA: HotStore[] = [
+  {
+    brandId: '1',
+    brandImgUrl: require('../../assets/img/introduce_background/introduce_background_1.png'),
+    brandName: 'Zara',
+    clothes: [
+      {
+        clothesId: '1.1',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_2.png'),
+      },
+      {
+        clothesId: '1.2',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_3.png'),
+      },
+      {
+        clothesId: '1.3',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.4',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.5',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+    ],
+  },
+  {
+    brandId: '2',
+    brandImgUrl: require('../../assets/img/introduce_background/introduce_background_2.png'),
+    brandName: 'Zara',
+    clothes: [
+      {
+        clothesId: '1.1',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_1.png'),
+      },
+      {
+        clothesId: '1.2',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_1.png'),
+      },
+      {
+        clothesId: '1.3',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.4',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.5',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+    ],
+  },
+  {
+    brandId: '3',
+    brandImgUrl: require('../../assets/img/introduce_background/introduce_background_3.png'),
+    brandName: 'Zara',
+    clothes: [
+      {
+        clothesId: '1.1',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_1.png'),
+      },
+      {
+        clothesId: '1.2',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_1.png'),
+      },
+      {
+        clothesId: '1.3',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.4',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+      {
+        clothesId: '1.5',
+        clothesImgUrl: require('../../assets/img/introduce_background/introduce_background_4.png'),
+      },
+    ],
+  },
+];
 
-const HOST_STORE_DATA = [
-    {
-        brandId: '1',
-        brandImgURL: '',
-        brandName: 'Zara',
-        clothes: [
-            {  clothesID: '1.1',
-            clothesImgURL: ''   
-            },
-            {  clothesID: '1.2',
-            clothesImgURL: ''   
-            },
+const HotStoreScreen = () => {
+  const [scrollUp, setScrollUp] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
-        ]
+  function hanldeGoBack(): void {
+    alert('back');
+  }
+
+  const handleScroll = (event: any) => {
+    const currentScrollPos = event.nativeEvent.contentOffset.y;
+
+    if (currentScrollPos > prevScrollPos) {
+      setScrollUp(false);
+    } else if (currentScrollPos < prevScrollPos) {
+      setScrollUp(true);
     }
-]
 
-const HotStoreScreen =  () => {
+    setPrevScrollPos(currentScrollPos);
+  };
 
-    function hanldeGoBack(): void {
-        alert('back')
-      }
-    
-      const handleSearch = () => {
-        alert('search')
-      }
-    
-      const handleMore = () => {
-        alert('handleMore')
-      }
-    return(
-        <View style={HotStoreStyleScreen.container}>
-        {/* <AppBarHeaderComponent
-          backAction={() => hanldeGoBack()}
-          iconChild={
-            <>
-              <Appbar.Action icon={'magnify'} onPress={handleSearch} />
-              <Appbar.Action icon='dots-vertical' onPress={handleMore} />
-            </>
-          }
-        ></AppBarHeaderComponent> */}
-  
-        <ScrollView
-          persistentScrollbar={false}
-          style={HotStoreStyleScreen.scrollView}
+  const handleSearch = () => {
+    alert('search');
+  };
+
+  const handleMore = () => {
+    alert('handleMore');
+  };
+  return (
+    <View style={HotStoreStyleScreen.container}>
+      <AppBarHeaderComponent
+        backAction={() => hanldeGoBack()}
+        title={
+          <View>
+            <MaskedView
+              maskElement={
+                <Text style={HotStoreStyleScreen.title}>HOT STORE</Text>
+              }
+            >
+              <LinearGradient
+                colors={[secondaryColor, primaryColor]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+                style={HotStoreStyleScreen.linearBackground}
+              >
+                <Text style={{ opacity: 0 }}>HOT STORE </Text>
+              </LinearGradient>
+            </MaskedView>
+          </View>
+        }
+        iconChild={
+          <>
+            <Appbar.Action icon={'magnify'} onPress={handleSearch} />
+            {/* <Appbar.Action icon='dots-vertical' onPress={handleMore} /> */}
+          </>
+        }
+      ></AppBarHeaderComponent>
+
+      <View style={HotStoreStyleScreen.scrollView}>
+        <FlatList
+          data={HOST_STORE_DATA}
+          style={[HotStoreStyleScreen.flatListColumn]}
+          keyExtractor={(store) => store.brandId}
           showsVerticalScrollIndicator={false}
           showsHorizontalScrollIndicator={false}
+          persistentScrollbar={false}
+          onScroll={(event) => handleScroll(event)}
           scrollEventThrottle={16}
-        >
-          <View style={HotStoreStyleScreen.scrollViewContent}>
+          renderItem={({ item }) => (
+            <View>
+              <View style={HotStoreStyleScreen.flatListRow}>
+                <Avatar.Image
+                  size={50}
+                  style={HotStoreStyleScreen.brandAvatar}
+                  source={require('../../assets/img/twitter_logo.png')}
+                />
+                <View style={HotStoreStyleScreen.star}>
+                  <IconFA name='star-o' size={25} color='black' />
+                  <Text style={HotStoreStyleScreen.numberOfStar}>200</Text>
+                </View>
 
+                <ListViewComponent
+                  data={item.clothes.map((clt) => {
+                    return {
+                      id: clt.clothesId,
+                      imgUrl: clt.clothesImgUrl,
+                    };
+                  })}
+                  isHorizontal={true}
+                  cardStyleContainer={HotStoreStyleScreen.card}
+                  cardStyleContent={HotStoreStyleScreen.cardImage}
+                />
+              </View>
 
-          </View>
-        </ScrollView>
-        </View>
-
-    )
-}
+              <Divider style={HotStoreStyleScreen.divider} bold />
+            </View>
+          )}
+        />
+      </View>
+      <AppBarFooterComponents isHide={scrollUp} centerIcon={'plus'} />
+    </View>
+  );
+};
 export default HotStoreScreen;
