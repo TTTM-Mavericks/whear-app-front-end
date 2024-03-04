3ab405f7b7c6bbfd14b58d8782f3439daced69b7
Thu Trang Le <lthutrang17@gmail.com>
2024-03-05 00:02:05 +0700

    [FE033] create onboarding screen

---
+++ src/Screens/Onboarding/Data.ts
@@ -0,0 +1,21 @@
+export const dataSliderOnboarding = [
+  [
+    'https://i.pinimg.com/564x/bb/a5/68/bba56867dc0ef6891b8779b262849f72.jpg',
+    'https://i.pinimg.com/564x/4e/36/a4/4e36a46ed27a8753a7915970bfc9f918.jpg',
+    'https://i.pinimg.com/564x/2d/a3/7d/2da37d34b3039fe6cf762f95886ac886.jpg',
+    'https://i.pinimg.com/736x/a0/ac/fd/a0acfd434b4cb7be5377c51478e1b9d0.jpg',
+  ],
+  [
+    'https://i.pinimg.com/564x/81/12/6f/81126f7bda0066a9487c3666a31a02f6.jpg',
+    'https://i.pinimg.com/564x/5f/4a/71/5f4a71b8b9c35540adc745b76b50a018.jpg',
+    'https://i.pinimg.com/564x/f3/c4/4f/f3c44f323f6ed91aebb36aaf2160b1b7.jpg',
+
+    'https://i.pinimg.com/564x/f0/19/bd/f019bd18fd13f420d923d04e181e37d3.jpg',
+  ],
+  [
+    'https://i.pinimg.com/736x/d8/c3/dd/d8c3ddbbaeda5dd6369ad69baf152f31.jpg',
+    'https://i.pinimg.com/564x/8f/21/8b/8f218b2af094e942f28d0095b625b453.jpg',
+    'https://i.pinimg.com/736x/f2/c4/00/f2c400ebf061cf94be3e5d8f9358e1c5.jpg',
+    'https://i.pinimg.com/564x/3f/ec/cd/3feccd9664bcb6c5aae9a36cd8a95832.jpg',
+  ],
+];
